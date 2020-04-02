09ef53fc56a85d5d2705922fb505e31eea5d8279
xihaoshangdi <xihaoshangdi@163.com>
2020-04-03 04:44:53 +0800

    增加了跨域访问的处理

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const user = require('../model/user');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   const param = req.query || req.params;
+  res.setHeader('Access-Control-Allow-Origin', '*');
   user.addUser().then(function(data) {
     if (!data.err) {
       const results = data.results;
