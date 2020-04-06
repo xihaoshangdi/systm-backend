cfa0eeb2772ffa7e81bd33cc30dadd6fe0cec7c2
xihaoshangdi <xihaoshangdi@163.com>
2020-04-06 22:55:00 +0800

    1. 重置了跨域的请求头 2. 更新了CheckToken模块 3. 快速返回预检请求Options

--- app.js
+++ app.js
@@ -18,17 +18,25 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(allowCors); //跨域中间件
 //路由
-app.use('/', indexRouter);
+app.get('/check', function (req, res, next) {
+  const token = req.headers.authorization;
+  console.log(token);
+
+  res.send(false);
+});
 app.use('/login', loginRouter);
+//check token
+// token过期bye
+app.use('/', indexRouter);
 app.use('/user', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- model/StudentCurd.js
+++ model/StudentCurd.js
@@ -1,21 +1,40 @@
 const pool = require('../Dao/operate');
-const userAddSql = `SELECT * FROM student`;
+const user1 = `select buildnumber, Dormitorynumber from student where grade=? or profession=?
+`;
+const user2 = `select buildnumber, Dormitorynumber from student where grade=? and profession=?
+`;
 const StudentOp = {
   findAllStudent(param) {
-    let promise = new Promise(function(resolve, reject) {
-      pool.query(userAddSql, param, function(err, results, fields) {
-        resolve({
-          err: err,
-          results: results,
-          fields: fields
-        });
-        reject({
-          err: err
-        });
-      });
+    if (Object.values(param).length === 2) {
+      const sql = user2;
+    } else {
+      const sql = user1;
+    }
+    pool.query(sql, [param.grade, param.profession], function (
+      err,
+      results,
+      fields
+    ) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(results);
+      }
     });
-    return promise;
-  }
+    // let promise = new Promise(function (resolve, reject) {
+    //   pool.query(userAddSql, param, function (err, results, fields) {
+    //     resolve({
+    //       err: err,
+    //       results: results,
+    //       fields: fields,
+    //     });
+    //     reject({
+    //       err: err,
+    //     });
+    //   });
+    // });
+    // return promise;
+  },
 };
 
 module.exports = StudentOp;
